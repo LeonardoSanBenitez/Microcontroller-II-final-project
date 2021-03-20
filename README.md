329a3efa4011f96dde7e977a4a1e20d943f6f469
Leonardo Benitez <lsbenitezpereira@gmail.com>
2021-03-20 15:07:24 -0300

    organizing responsabilities among the classes

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # Microcontroller-II-final-project
+
+The plan is to have a player blob (blue), which aims to navigate its way as quickly as possible to the food blob (green), while avoiding the enemy blob (red).
+
+ our environment will be a 20 x 20 grid, where we have 1 player, 1 enemy, and 1 food. For now, we'll just have the player able to move
+
+A 10x10 Q-Table for example, in this case, is ~15MB. A 20x20 is ~195MB
+I wanted to see how much of a difference going from 10x10 to a 20x20 would make. As we saw, somewhere between 25k and 75K training episodes were required for the 10x10 to learn. I found it took more like 2.5 million episodes for a 20x20 model to learn.
+
+
+# TODO STM
+* Rodar um hello world com OO
+* Alocar um vetor de 4*(2*2 -1)^4 inteiros com sinal
+
+# TODO RL
+* Mudar tamanho Q table
+* desassociar Q-table do state global
+* Desacoplar agent, environment e display
+
+# Formulação da Q table
+we simply pass the x and y deltas for the food and enemy to our agent.
+4 actions
+dicionário `[[s0,s1],[s2,s3]] -> [a0, a1, a2, a3]`
+size 3 -> len(q) = 625 estados possíveis (keys) = (SIZE*2 -1)**4
+
+# Experiment log
+## Initial
+on ep. 0, mean reward is nan (epsilon is 0.900)
+on ep. 3000, mean reward is -174.30 (epsilon is 0.494)
+on ep. 6000, mean reward is -113.65 (epsilon is 0.271)
+on ep. 9000, mean reward is -84.33 (epsilon is 0.149)
+on ep. 12000, mean reward is -70.38 (epsilon is 0.082)
+on ep. 15000, mean reward is -58.04 (epsilon is 0.045)
+on ep. 18000, mean reward is -48.46 (epsilon is 0.025)
+on ep. 21000, mean reward is -42.77 (epsilon is 0.013)
+on ep. 24000, mean reward is -36.74 (epsilon is 0.007)
+
+## New actions
+on ep. 0, mean reward is nan (epsilon is 0.900)
+on ep. 3000, mean reward is -179.33 (epsilon is 0.494)
+on ep. 6000, mean reward is -130.48 (epsilon is 0.271)
+on ep. 9000, mean reward is -88.94 (epsilon is 0.149)
+on ep. 12000, mean reward is -63.96 (epsilon is 0.082)
+on ep. 15000, mean reward is -47.53 (epsilon is 0.045)
+on ep. 18000, mean reward is -38.93 (epsilon is 0.025)
+on ep. 21000, mean reward is -33.63 (epsilon is 0.013)
+on ep. 24000, mean reward is -24.95 (epsilon is 0.007)
+
+## Size 3
+on ep. 0, mean reward is nan (epsilon is 0.900)
+on ep. 3000, mean reward is -90.15 (epsilon is 0.494)
+on ep. 6000, mean reward is -43.20 (epsilon is 0.271)
+on ep. 9000, mean reward is -25.77 (epsilon is 0.149)
+on ep. 12000, mean reward is -19.61 (epsilon is 0.082)
+on ep. 15000, mean reward is -13.44 (epsilon is 0.045)
+on ep. 18000, mean reward is -7.70 (epsilon is 0.025)
+on ep. 21000, mean reward is -5.25 (epsilon is 0.013)
+on ep. 24000, mean reward is -1.26 (epsilon is 0.007)
+
+## Size 3, no special final reward, longer training
+on ep. 3000, mean reward is -91.97 (epsilon is 0.494)
+on ep. 6000, mean reward is -51.47 (epsilon is 0.271)
+on ep. 9000, mean reward is -31.59 (epsilon is 0.149)
+on ep. 12000, mean reward is -20.12 (epsilon is 0.082)
+on ep. 15000, mean reward is -15.97 (epsilon is 0.045)
+on ep. 18000, mean reward is -4.86 (epsilon is 0.025)
+on ep. 21000, mean reward is -5.68 (epsilon is 0.013)
+on ep. 24000, mean reward is -1.78 (epsilon is 0.007)
+on ep. 27000, mean reward is -4.25 (epsilon is 0.004)
--- game-local/main.py
+++ game-local/main.py
@@ -1,60 +1,3 @@
-'''
-The plan is to have a player blob (blue), which aims to navigate its way as quickly as possible to the food blob (green), while avoiding the enemy blob (red).
-
-
- our environment will be a 20 x 20 grid, where we have 1 player, 1 enemy, and 1 food. For now, we'll just have the player able to move
-
-A 10x10 Q-Table for example, in this case, is ~15MB. A 20x20 is ~195MB
-I wanted to see how much of a difference going from 10x10 to a 20x20 would make. As we saw, somewhere between 25k and 75K training episodes were required for the 10x10 to learn. I found it took more like 2.5 million episodes for a 20x20 model to learn.
-
-
-
-# TODO
-* Mudar tamanho Q table
-* desassociar Q-table do state global
-* Desacoplar agent, environment e display
-
-# Formulação da Q table
-we simply pass the x and y deltas for the food and enemy to our agent.
-4 actions
-dicionário `[[s0,s1],[s2,s3]] -> [a0, a1, a2, a3]`
-size 3 -> len(q) = 625 estados possíveis (keys) = (SIZE*2 -1)**4
-
-# Experiment log
-## Initial
-on ep. 0, mean reward is nan (epsilon is 0.900)
-on ep. 3000, mean reward is -174.30 (epsilon is 0.494)
-on ep. 6000, mean reward is -113.65 (epsilon is 0.271)
-on ep. 9000, mean reward is -84.33 (epsilon is 0.149)
-on ep. 12000, mean reward is -70.38 (epsilon is 0.082)
-on ep. 15000, mean reward is -58.04 (epsilon is 0.045)
-on ep. 18000, mean reward is -48.46 (epsilon is 0.025)
-on ep. 21000, mean reward is -42.77 (epsilon is 0.013)
-on ep. 24000, mean reward is -36.74 (epsilon is 0.007)
-
-## New actions
-on ep. 0, mean reward is nan (epsilon is 0.900)
-on ep. 3000, mean reward is -179.33 (epsilon is 0.494)
-on ep. 6000, mean reward is -130.48 (epsilon is 0.271)
-on ep. 9000, mean reward is -88.94 (epsilon is 0.149)
-on ep. 12000, mean reward is -63.96 (epsilon is 0.082)
-on ep. 15000, mean reward is -47.53 (epsilon is 0.045)
-on ep. 18000, mean reward is -38.93 (epsilon is 0.025)
-on ep. 21000, mean reward is -33.63 (epsilon is 0.013)
-on ep. 24000, mean reward is -24.95 (epsilon is 0.007)
-
-## Size 3
-on ep. 0, mean reward is nan (epsilon is 0.900)
-on ep. 3000, mean reward is -90.15 (epsilon is 0.494)
-on ep. 6000, mean reward is -43.20 (epsilon is 0.271)
-on ep. 9000, mean reward is -25.77 (epsilon is 0.149)
-on ep. 12000, mean reward is -19.61 (epsilon is 0.082)
-on ep. 15000, mean reward is -13.44 (epsilon is 0.045)
-on ep. 18000, mean reward is -7.70 (epsilon is 0.025)
-on ep. 21000, mean reward is -5.25 (epsilon is 0.013)
-on ep. 24000, mean reward is -1.26 (epsilon is 0.007)
-
-'''
 import numpy as np
 from PIL import Image
 import cv2
@@ -63,34 +6,22 @@ import pickle
 from matplotlib import style
 import time
 
-style.use("ggplot")
-
 SIZE = 3
-
-HM_EPISODES = 25000
-MOVE_PENALTY = 1
-ENEMY_PENALTY = 300
-FOOD_REWARD = 25
-epsilon = 0.9
-EPS_DECAY = 0.9998  # Every episode will be epsilon*EPS_DECAY
+HM_EPISODES = 30000
 LOG_EVERY = 3000  # how often to play through env visually.
-SHOW_EVERY = 6000
+SHOW_EVERY = 40000
 start_q_table = None # None or Filename
 
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
 
-PLAYER_N = 1  # player key in dict
-FOOD_N = 2  # food key in dict
-ENEMY_N = 3  # enemy key in dict
 
-# the dict!
-d = {1: (255, 175, 0),
-     2: (0, 255, 0),
-     3: (0, 0, 255)}
 
 
 class IntelligentAgent():
+    LEARNING_RATE = 0.15
+    DISCOUNT = 0.95
+    epsilon = 0.9
+    EPS_DECAY = 0.9998
+
     def __init__(self, start_q_table):
         if start_q_table is None:
             self.q_table = {}
@@ -104,10 +35,12 @@ class IntelligentAgent():
             with open(start_q_table, "rb") as f:
                 self.q_table = pickle.load(f)
 
+        print('[AI] Init object with Q table size of %d elements'%(len(self.q_table)*len(list(self.q_table.values())[0])))
+
         super().__init__()
 
     def action(self, obs):
-        if np.random.random() > epsilon:
+        if np.random.random() > self.epsilon:
             action = np.argmax(self.q_table[obs])
         else:
             action = np.random.randint(0, 4)
@@ -118,17 +51,15 @@ class IntelligentAgent():
         max_future_q = np.max(self.q_table[new_obs])
         current_q = self.q_table[obs][action]
 
-        if reward == FOOD_REWARD:
-            new_q = FOOD_REWARD
-        else:
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+        new_q = (1 - self.LEARNING_RATE) * current_q + self.LEARNING_RATE * (reward + self.DISCOUNT * max_future_q)
         self.q_table[obs][action] = new_q
 
     def to_disk(self, name):
         with open(name, "wb") as f:
             pickle.dump(self.q_table, f)
 
-
+    def episode_callback(self):
+        self.epsilon *= self.EPS_DECAY
 
 # can look up from Q-table with: print(self.q_table[((-9, -2), (3, 9))]) for example
 class Env():
@@ -174,9 +105,9 @@ class Blob:
         self.x = 0
         self.y = 0
 
-    def reset(self):
-        self.x = np.random.randint(0, SIZE)
-        self.y = np.random.randint(0, SIZE)
+    def set_pos(self, x, y):
+        self.x = x
+        self.y = y
 
     def __str__(self):
         return f"{self.x}, {self.y}"
@@ -191,6 +122,20 @@ class Blob:
 class EnvBlob(Env):
     action_space = None
     observation_space = None
+
+    PLAYER_N = 1  # player key in dict
+    FOOD_N = 2  # food key in dict
+    ENEMY_N = 3  # enemy key in dict
+
+    MOVE_PENALTY = 1
+    ENEMY_PENALTY = 300
+    FOOD_REWARD = 25
+
+    # Color dict
+    d = {1: (255, 175, 0),
+        2: (0, 255, 0),
+        3: (0, 0, 255)}
+
     def __init__(self):
         self.player = Blob()
         self.food = Blob()
@@ -198,9 +143,9 @@ class EnvBlob(Env):
         super().__init__()
 
     def reset(self):
-        self.player.reset()
-        self.food.reset()
-        self.enemy.reset()
+        self.player.set_pos(np.random.randint(0, SIZE), np.random.randint(0, SIZE))
+        self.food.set_pos(np.random.randint(0, SIZE), np.random.randint(0, SIZE))
+        self.enemy.set_pos(np.random.randint(0, SIZE), np.random.randint(0, SIZE))
         self._step_counter = 0
         self._done = False
         #TODO: dont generate levels with colisions 
@@ -221,16 +166,16 @@ class EnvBlob(Env):
 
         # Reward function
         if self.player.x == self.enemy.x and self.player.y == self.enemy.y:
-            reward = -ENEMY_PENALTY
+            reward = -self.ENEMY_PENALTY
         elif self.player.x == self.food.x and self.player.y == self.food.y:
-            reward = FOOD_REWARD
+            reward = self.FOOD_REWARD
         else:
-            reward = -MOVE_PENALTY
+            reward = -self.MOVE_PENALTY
 
         observation = (self.player-self.food, self.player-self.enemy)
 
         self._step_counter += 1
-        if self._step_counter>=199 or reward == FOOD_REWARD or reward == -ENEMY_PENALTY:
+        if self._step_counter>=199 or reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY:
             self._done = True
         else:
             self._done = False
@@ -240,9 +185,9 @@ class EnvBlob(Env):
 
     def render(self):
         grid = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
-        grid[self.food.x][self.food.y] = d[FOOD_N]  # sets the food location tile to green color
-        grid[self.player.x][self.player.y] = d[PLAYER_N]  # sets the player tile to blue
-        grid[self.enemy.x][self.enemy.y] = d[ENEMY_N]  # sets the enemy location to red
+        grid[self.food.x][self.food.y] = self.d[self.FOOD_N]  # sets the food location tile to green color
+        grid[self.player.x][self.player.y] = self.d[self.PLAYER_N]  # sets the player tile to blue
+        grid[self.enemy.x][self.enemy.y] = self.d[self.ENEMY_N]  # sets the enemy location to red
         img = Image.fromarray(grid, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
         img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
         cv2.imshow("image", np.array(img))  # show it!
@@ -253,17 +198,8 @@ class EnvBlob(Env):
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 return None
 
-### Main
-#env = wrap_env(gym.make("Atlantis-v0"))
-# for observation:
-#     observation = env.reset()
-#     while True:
-#         env.render()
-#         action = agent(observation)
-#         observation, reward, done, info = env.step(action) 
-#         if done: 
-#           break;
-            
+
+           
 episode_rewards = []
 AI = IntelligentAgent(start_q_table=start_q_table)
 env = EnvBlob()
@@ -291,30 +227,11 @@ for episode in range(HM_EPISODES):
 
 
     if episode % LOG_EVERY == 0:
-        print("on ep. %d, mean reward is %.2f (epsilon is %.3f)"%(episode, np.mean(episode_rewards[-LOG_EVERY:]), epsilon))
+        print("on ep. %d, mean reward is %.2f (epsilon is %.3f)"%(episode, np.mean(episode_rewards[-LOG_EVERY:]), AI.epsilon))
 
     episode_rewards.append(episode_reward)
-    epsilon *= EPS_DECAY
-
-class Blob:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-
-    def reset(self):
-        self.x = np.random.randint(0, SIZE)
-        self.y = np.random.randint(0, SIZE)
-
-    def __str__(self):
-        return f"{self.x}, {self.y}"
-
-    def __sub__(self, other):
-        return (self.x-other.x, self.y-other.y)
-
-    def move(self, x, y):
-        self.x += x
-        self.y += y
-
+    AI.episode_callback()
+    
 moving_avg = np.convolve(episode_rewards, np.ones((LOG_EVERY,))/LOG_EVERY, mode='valid')
 
 plt.plot([i for i in range(len(moving_avg))], moving_avg)
