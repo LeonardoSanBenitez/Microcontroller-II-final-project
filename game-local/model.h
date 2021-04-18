457c788f40b337f849215c5e05b643aaed77de01
Leonardo Benitez <lsbenitezpereira@gmail.com>
2021-04-18 00:56:48 -0300

    saving q_table to header file

--- game-local/model.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef MODEL
-#define MODEL
- 
-#define LEN 2
-int q_table [LEN][LEN] = {{1,2}, {3,4}};
- 
-#endif
--- game-local/trained-models/long training/game_config.h
+++ game-local/trained-models/long training/game_config.h
@@ -1,5 +1,5 @@
-#define OBS_SIZE 3
 #define OBS_RANGE OBS_SIZE*2 -1
+#define OBS_SIZE 3
 #define GRID_SIZE 10
 //#define DEPLOYED
 
@@ -10,17 +10,17 @@
 	#define LOG_EVERY 1
     #define HM_EPISODES 30
 #else
-	#define INITIAL_EPSILON 30
+	#define INITIAL_EPSILON 90
 	#define SHOW_EVERY HM_EPISODES + 1
-	#define LOG_EVERY 6000
-    #define HM_EPISODES 300000
+	#define LOG_EVERY 300000 //3000
+    #define HM_EPISODES 30000000 //30000
 #endif
  
 
-#define LOAD_Q_TABLE
+
 #define ACTION_SPACE 4
 
-#define LEARNING_RATE   0.00
+#define LEARNING_RATE   0.1
 #define DISCOUNT        0.2
 #define EPS_DECAY       0.999995
 
